1236666bd7deb9839f7014c3b07f08c27ae261f2
stevedowpe <stevedowpe@gmail.com>
2018-07-19 15:18:26 -0700

    Add files post tutor session

--- plots_bar.js
+++ plots_bar.js
@@ -1,162 +1,144 @@
 // (1) READ IN CSV FILE
 
 Plotly.d3.csv("SDCCD-Program-Close-Data.csv", function (error, response) {
-      console.log('RESPONSE:', response);
-      ////////////////////////////////////////
-      ////////////////////////////////////////
-      ///////////////////////////////////////
+  console.log('RESPONSE:', response);
+  ////////////////////////////////////////
+  ////////////////////////////////////////
+  ///////////////////////////////////////
 
-      // (2) DSA CHART
-      // (2-A) SET BLANK VARIABLE ARRAYS
-      var yesCount_DSA = 0
-      var noCount_DSA = 0
-      var okCount_DSA = 0
-      var naCount_DSA = 0
-      var tbdCount_DSA = 0
+  // (2) DSA CHART
+  // (2-A) SET BLANK VARIABLE ARRAYS
+  var yesCount_DSA = 0
+  var noCount_DSA = 0
+  var okCount_DSA = 0
+  var naCount_DSA = 0
+  var tbdCount_DSA = 0
 
-      var status_DSA = [];
-      var DSANumber = [];
-      var ProjectDescription = [];
+  var status_DSA = [];
+  var DSANumber = [];
+  var DSANumber_yes = [];
+  var DSANumber_no = [];
+  var DSANumber_ok = [];
+  var DSANumber_na = [];
+  var DSANumber_tbd = [];
 
-      ///////////////////////////////////////
-      // (2-B) LOOP THROUGH RESPONSES AND PUSH VALUES TO ARRAYS
-      for (var i = 0; i < response.length; i++) {
-        if (response[i].DSA == "Yes") {
-          yesCount_DSA += 1
-        } else if (response[i].DSA == "No") {
-          noCount_DSA += 1
-        } else if (response[i].DSA == "OK") {
-          okCount_DSA += 1
-        } else if (response[i].DSA == "N A") {
-          naCount_DSA += 1
-        } else {
-          tbdCount_DSA += 1
-        }
-        status_DSA.push(response[i].DSA)
-        DSANumber.push(response[i].DSANumber)
-        ProjectDescription.push(response[i].ProjectDescription)
-      };
-
-      ///////////////////////////////////////
-      // (2-C) MISC INFO TO CONSOLE LOG
-      console.log('DSA Status : ', status_DSA)
-      console.log('DSA Number: ', DSANumber)
-      console.log('Project Description: ', ProjectDescription)
-
-      ///////////////////////////////////// 
-      // (2-D?) SET PARTIAL INFO FOR CLICK-EVENT
-      var myPlot = document.getElementById('plot_DSA_bar');
-
-      ///////////////////////////////////// 
-      // (2-E) SET BAR CHART DATA AND LAYOUT... AND RUN PLOTLY  (DSA)
-      var trace_DSA = {
-        x: ["Yes", "N A", "TBD", "OK", "No"],
-        y: [yesCount_DSA, naCount_DSA, tbdCount_DSA, okCount_DSA, noCount_DSA],
-        marker: {
-          color: ['green', 'grey', 'royalblue', 'deeppink', 'crimson']
-        },
-        type: 'bar'
-      };
-
-      var data_DSA = [trace_DSA];
-
-      var layout_DSA = {
-        title: "DSA Cert Status",
-        height: 350,
-        width: 350,
-      };
-      Plotly.newPlot("plot_DSA_bar", data_DSA, layout_DSA);
-
-      ///////////////////////////////////// 
-      // (2-F) ATTEMPT TO BIND A POP-UP TO THE BAR CHART (DSA)
-
-      myPlot.on('plotly_click', function (data) {
-        var pts = '';
-        for (var i = 0; i < data.points.length; i++) {
-          pts = 'x = ' + data.points[i].x +
-            '\ny = ' + data.points[i].y.toPrecision(2) +
-            '\nDSA # = ' + ProjectDescription[10] +
-            '\n\n';
-        }
-        alert('Closest point clicked:\n\n' + pts);
-      });
-
-      // function popuptable(){ 
+  var ProjectDescription = [];
+  var ProjectDescription_yes = [];
+  var ProjectDescription_no = [];
+  var ProjectDescription_ok = [];
+  var ProjectDescription_na = [];
+  var ProjectDescription_tbd = [];
 
 
-      ////////////////////////////////////////
-      ////////////////////////////////////////
-      ///////////////////////////////////////
-
-      // (3) LEED CHART
-      // (3-A) SET BLANK VARIABLE ARRAYS
-      var yesCount_LEED = 0
-      var noCount_LEED = 0
-      var okCount_LEED = 0
-      var naCount_LEED = 0
-      var tbdCount_LEED = 0
-
-      var status_LEED = [];
-
-
-      ///////////////////////////////////////
-      // (3-B) LOOP THROUGH RESPONSES AND PUSH VALUES TO ARRAYS
-      for (var i = 0; i < response.length; i++) {
-        if (response[i].LEED == "Yes") {
-          yesCount_LEED += 1
-        } else if (response[i].LEED == "No") {
-          noCount_LEED += 1
-        } else if (response[i].LEED == "OK") {
-          okCount_LEED += 1
-        } else if (response[i].LEED == "N A") {
-          naCount_LEED += 1
-        } else {
-          tbdCount_LEED += 1
-        }
-        status_LEED.push(response[i].LEED)
-      };
-
-      ///////////////////////////////////////
-      // (3-C) MISC INFO TO CONSOLE LOG
-      console.log('LEED Status : ', status_LEED)
+  ///////////////////////////////////////
+  // (2-B1) LOOP THROUGH RESPONSES AND PUSH VALUES TO ARRAYS
+  for (var i = 0; i < response.length; i++) {
+    if (response[i].DSA == "Yes") {
+      yesCount_DSA += 1
+      ProjectDescription_yes.push(response[i].ProjectDescription)
+      DSANumber_yes.push(response[i].DSANumber)
+    } else if (response[i].DSA == "No") {
+      noCount_DSA += 1
+      ProjectDescription_no.push(response[i].ProjectDescription)
+      DSANumber_no.push(response[i].DSANumber)
+    } else if (response[i].DSA == "OK") {
+      okCount_DSA += 1
+      ProjectDescription_ok.push(response[i].ProjectDescription)
+      DSANumber_ok.push(response[i].DSANumber)
+    } else if (response[i].DSA == "N A") {
+      naCount_DSA += 1
+      ProjectDescription_na.push(response[i].ProjectDescription)
+      DSANumber_na.push(response[i].DSANumber)
+    } else {
+      tbdCount_DSA += 1
+      ProjectDescription_tbd.push(response[i].ProjectDescription)
+      DSANumber_tbd.push(response[i].DSANumber)
+    }
+    status_DSA.push(response[i].DSA)
+    DSANumber.push(response[i].DSANumber)
+    ProjectDescription.push(response[i].ProjectDescription)
+  };
 
 
-      ///////////////////////////////////// 
-      // (3-D?) SET PARTIAL INFO FOR CLICK-EVENT
-      var myPlot = document.getElementById('plot_LEED_bar');
+  console.log('Project Description: ', ProjectDescription)
+  console.log('Project Description_No: ', ProjectDescription_no)
+  console.log('Project Description_Yes: ', ProjectDescription_yes)
+  console.log('DSA Number, YES: ', DSANumber_yes )
 
-      ///////////////////////////////////// 
-      // (3-E) SET BAR CHART DATA AND LAYOUT... AND RUN PLOTLY  (LEED)
-      var trace_LEED = {
-        x: ["Yes", "N A", "TBD", "OK", "No"],
-        y: [yesCount_LEED, naCount_LEED, tbdCount_LEED, okCount_LEED, noCount_LEED],
-        marker: {
-          color: ['green', 'grey', 'royalblue', 'deeppink', 'crimson']
-        },
-        type: 'bar'
-      };
+  ///////////////////////////////////// 
+  // (2-D?) SET PARTIAL INFO FOR CLICK-EVENT
+  var myPlot = document.getElementById('plot_DSA_bar');
 
-      var data_LEED = [trace_LEED];
+  ///////////////////////////////////// 
+  // (2-E) SET BAR CHART DATA AND LAYOUT... AND RUN PLOTLY  (DSA)
+  var trace_DSA = {
+    x: ["Yes", "N A", "TBD", "OK", "No"],
+    y: [yesCount_DSA, naCount_DSA, tbdCount_DSA, okCount_DSA, noCount_DSA],
+    marker: {
+      color: ['green', 'grey', 'royalblue', 'deeppink', 'crimson']
+    },
+    type: 'bar'
+  };
 
-      var layout_LEED = {
-        title: "LEED Cert Status",
-        height: 350,
-        width: 350,
-      };
-      Plotly.newPlot("plot_LEED_bar", data_LEED, layout_LEED);
+  var data_DSA = [trace_DSA];
 
-      ///////////////////////////////////// 
-      // (3-F) ATTEMPT TO BIND A POP-UP TO THE BAR CHART (LEED)
+  var layout_DSA = {
+    title: "DSA Cert Status",
+    height: 350,
+    width: 350,
+  };
+  Plotly.newPlot("plot_DSA_bar", data_DSA, layout_DSA);
 
-      myPlot.on('plotly_click', function (data) {
-        var pts = '';
-        for (var i = 0; i < data.points.length; i++) {
-          pts = 'x = ' + data.points[i].x +
-            '\ny = ' + data.points[i].y.toPrecision(2) +
-            '\n\n';
-        }
-        alert('Closest point clicked:\n\n' + pts);
-      });
+  myPlot.on('plotly_click', function (data) {
+    console.log(data)
+    var pts = '';
+    var yAxis = data.points[0].y;
+    // get the index of y-value
+    var xAxisIndex = data.points[0].data.y.indexOf(yAxis);
+    // with that index I calculate x-value
+    var xAxis = data.points[0].data.x[xAxisIndex]
+    console.log(xAxis);
+    if (xAxis == "Yes") {
+      ProjectDescription_x = ProjectDescription_yes
+      DSANumber_x = DSANumber_yes
+      console.log(ProjectDescription_yes)
+    }
+    else if (xAxis == "N A") {
+      ProjectDescription_x = ProjectDescription_na
+      DSANumber_x = DSANumber_na     
+      console.log(ProjectDescription_na)
+    }
+    else if (xAxis == "TBD") {
+      ProjectDescription_x = ProjectDescription_tbd
+      DSANumber_x = DSANumber_tbd     
+      console.log(ProjectDescription_tbd)
+    }
+    else if (xAxis == "OK") {
+      ProjectDescription_x = ProjectDescription_ok
+      DSANumber_x = DSANumber_ok   
+      console.log(ProjectDescription_ok)
+    }
+    else if (xAxis == "No") {
+      ProjectDescription_x = ProjectDescription_no
+      DSANumber_x = DSANumber_no   
+      console.log(ProjectDescription_no)
+    }
+    else {
+      "Hello"
+    }
 
-      // function popuptable(){ 
+    var table_alert = '<table>'
+    table_alert +="<th>DSA Number</th>", "<th>Project Names</th>"
+    for (var i = 0; i < ProjectDescription_x.length; i++) {
+      table_alert += "<tr>";
+      table_alert += "<td>" + DSANumber_x[i] + "</td>";
+      table_alert += "<td>" + ProjectDescription_x[i] + "</td>";
+      table_alert += "</tr>";
+    }
+    table_alert += "</table>";
+
+    document.getElementById("populating_table").innerHTML = table_alert;
+  })
+  
 });
+
